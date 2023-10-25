0a9204044872527b8113205d7e8a12af79780293
martin <martbargar@gmail.com>
2023-10-25 09:49:36 -0600

    Login Funcional

--- login.php
+++ login.php
@@ -4,33 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="recursos/styles.css">
-    <link rel="stylesheet" href="recursos/preloader.css">
-   
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <div class="login-box">
         <p>Ingresar</p>
-        <form>
+          <?php 
+          include("conexion.php");
+          include("controlador.php");
+    
+          ?>
+        <form method="post">
           <div class="user-box">
-            <input required="" name="" type="text">
+            <input required="" name="usuario" type="text">
             <label>Usuario</label>
           </div>
           <div class="user-box">
-            <input required="" name="" type="password">
+            <input required="" name="password" type="password">
             <label>Contraseña</label>
           </div>
-          <a href="#">
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span>
-            Enviar
-          </a>
+          <input type="submit" value="Enviar" name="btningresar">
         </form>
         <!--<p>Don't have an account? <a href="" class="a2">Sign up!</a></p>-->
       </div>
-      <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-      <script src="recursos/preloader.js"></script>
 </body>
 </html>
